437bd6a15e235d10c2208ed00520b5ba03124b35
wsdma <fd.wsdma@gmail.com>
2024-12-12 18:10:20 +0200

    remove apiUrl from env file

--- src/core/src/environments/environment.ts
+++ src/core/src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '../interfaces/environment';
 
 export const environment: Environment = {
-  openWeatherApiKey: 'b1a846571092553aa3a96455ed21f279',
+  openWeatherApiKey: null,
   apiUrl: 'https://api.openweathermap.org',
 };
